77b10ab85e90d9e660d19eab8648900e5d19c3a4
StackOverflow <96837517+stopperbir@users.noreply.github.com>
2025-08-22 23:19:53 +0300

    Update txqueuelen.sh

--- txqueuelen.sh
+++ txqueuelen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #!ifconfig bond0 txqueuelen 20000 && ifconfig eno12399 txqueuelen 20000 && ifconfig eno12409 txqueuelen 20000 && ifconfig lo txqueuelen 20000
-blockdev --setra 4096 /dev/md0 && blockdev --setra 4096 /dev/md1 && blockdev --setra 4096 /dev/md2 && blockdev --setra 4096 /dev/nvme0n1 && blockdev --setra 4096 /dev/nvme1n1
+blockdev --setra 4096 /dev/md0 && blockdev --setra 4096 /dev/md1 && blockdev --setra 4096 /dev/md2 && blockdev --setra 4096 /dev/nvme0n1 && blockdev --setra 4096 /dev/nvme1n1 && blockdev --report
